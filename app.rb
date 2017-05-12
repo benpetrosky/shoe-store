4feebf048e6d059b44043ae21f403184fca6e957
Ben Petrosky <benpetrosky@hotmail.com>
2017-05-12 10:58:23 -0700

    displays the shoe brands that a store has and allows user to update the brands at a store if the brand is in the database.

--- app.rb
+++ app.rb
@@ -43,6 +43,8 @@ end
 get("/stores/:id") do
   id = params.fetch("id").to_i
   @store = Store.find(id)
+  @brands = Brand.all()
+  @store_brands = @store.brands()
   erb(:store)
 end
 
@@ -57,7 +59,18 @@ end
 patch('/store_update/:id') do
   id = params.fetch("id").to_i
   name = params.fetch("name")
+  brand_ids = params.fetch("brand_ids", "")
+  @brands = Brand.all()
+
   @store = Store.find(id)
+  @store_brands = @store.brands()
   @store.update(:name => name)
+
+  if brand_ids != ""
+    brand_ids.each() do |brand_id|
+      brand = Brand.find(brand_id)
+      @store_brands = @store.brands.push(brand)
+    end
+  end
   erb(:store)
 end
